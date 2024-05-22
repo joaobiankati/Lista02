352aa898e6b34655fd450aed1a4a20993f2855b0
João Biankati de Souza <joaobiankati0164@gmail.com>
2024-05-22 01:35:40 -0300

    Lista02

--- ex03.py
+++ ex03.py
@@ -3,7 +3,7 @@ gols_time_b = int(input("Informe a quantidade de gols do time B: "))
 
 if gols_time_a > gols_time_b:
     print(f"O time A venceu a partida!")
-if gols_time_b > gols_time_a:
+elif gols_time_b > gols_time_a:
     print(f"O time B venceu a partida!")
-if gols_time_a == gols_time_b:
+else:
     print(f"A partidade ficou empatada!")
